0dd1b429b05450d8bf9f9d33366136e7f97d773a
Akmal Rakhimov <arakhimoff@gmail.com>
2019-05-05 18:25:46 -0400

    Removed unnecessary annotations.

--- src/main/kotlin/com/example/springkotlindemo/SpringKotlinDemoApplication.kt
+++ src/main/kotlin/com/example/springkotlindemo/SpringKotlinDemoApplication.kt
@@ -1,13 +1,9 @@
 package com.example.springkotlindemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = arrayOf("com.example.springkotlindemo.*"))
-@EntityScan("com.example.springkotlindemo.*")
 class SpringKotlinDemoApplication
 
 fun main(args: Array<String>) {
